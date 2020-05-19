d8ad9e51abedab6afc712d77aaef48aa1fcd56db
Kacperk6 <krejuchna12@gmail.com>
2020-05-19 18:45:00 +0200

    added split_stereo_image function

--- utils/camera.py
+++ utils/camera.py
@@ -11,3 +11,16 @@ def get_video_from_file(file_name):
 
 def get_image(file_path):
     return cv2.imread(file_path)
+
+
+def split_stereo_image(stereo_image, height, width):
+    """
+    splits image from stereo camera to 2 separate images
+    :param stereo_image: image to split
+    :param height: input image height
+    :param width: input image width
+    :return: [frame_left, frame_right]: split input image
+    """
+    frame_left = stereo_image[0:height, 0:int(width/2)]
+    frame_right = stereo_image[0:height, int(width / 2): width]
+    return [frame_left, frame_right]
