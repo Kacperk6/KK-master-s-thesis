4b5eaff38edcf5c47249e55bc47b779833fb90d1
Kacperk6 <krejuchna12@gmail.com>
2020-07-15 15:57:14 +0200

    changed stereoscopy name to (proper) stereo vision. again

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def run():
     logging.info("program started")
 
     yolact = YolactFacade()
-    stereoscopy = StereoVision(calibrate_stereo_matching=True)
+    stereo_vision = StereoVision(calibrate_stereo_matching=True)
 
     cap = camera.get_video_live()
     while True:
         _, img_double = cap.read()
-        img_l, img_r = stereoscopy.preprocess_stereo_image(img_double, img_double.shape[0], img_double.shape[1])
+        img_l, img_r = stereo_vision.preprocess_stereo_image(img_double, img_double.shape[0], img_double.shape[1])
         cv2.imshow("img_l", img_l)
 
         mask = yolact.run(img_l, 'person')
@@ -34,12 +34,12 @@ def run():
             cv2.imwrite('img_l.png', img_l)
             cv2.imwrite("img_r.png", img_r)
         elif key == ord('d'):
-            depth_map, points_3d = stereoscopy.run(img_l, img_r)
+            depth_map, points_3d = stereo_vision.run(img_l, img_r)
             if mask is not None:
-                points_3d, depth_map, img_l = stereoscopy.mask_3d(mask, points_3d, depth_map, img_l)
+                points_3d, depth_map, img_l = stereo_vision.mask_3d(mask, points_3d, depth_map, img_l)
             depth_map_img = depth_map / 128
             cv2.imshow("depth", depth_map_img)
-            stereoscopy.draw_point_cloud(points_3d, depth_map, img_l)
+            stereo_vision.draw_point_cloud(points_3d, depth_map, img_l)
 
     cap.release()
     cv2.destroyAllWindows()
