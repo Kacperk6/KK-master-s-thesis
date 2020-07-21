e3fb77e34a900c3642f9287ede173249962d7f82
Kacperk6 <krejuchna12@gmail.com>
2020-07-21 22:24:19 +0200

    added get_grasp_orientation(), fixed decimate_contour(), changed some instance attribute reference for speed

--- shape_analysis/shape_analysis.py
+++ shape_analysis/shape_analysis.py
@@ -1,10 +1,10 @@
 import numpy as np
 import cv2
+import math
 
 
 class ShapeAnalyzer:
     def __init__(self):
-        self.contour = None
         self.centroid = None
         self.cont_len = None
 
@@ -17,14 +17,15 @@ class ShapeAnalyzer:
         """
         mask = mask.astype('uint8')
         contours, _ = cv2.findContours(mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-        self.contour = contours[0]
-        self.get_contour_parameters()
-        return contours[0]
+        contour = contours[0]
+        self.get_contour_parameters(contour)
+        return contour
 
     def analyze_shape(self, mask):
-        self.make_contour(mask)
-        self.draw_contour(self.contour, (mask.shape[0], mask.shape[1]))
-        grasp_points = self.get_grasp_points()
+        contour = self.make_contour(mask)
+        self.draw_contour(contour, (mask.shape[0], mask.shape[1]))
+        contour = self.decimate_contour(contour)
+        grasp_points = self.get_grasp_points(contour)
 
     @staticmethod
     def draw_contour(contour, img_size):
@@ -33,55 +34,69 @@ class ShapeAnalyzer:
         """
         img = np.zeros(img_size, np.uint8)
         img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-        cv2.drawContours(img, [contour], -1, (0, 255, 0), 3)
+        cv2.drawContours(img, contour, -1, (0, 255, 0), 3)
         cv2.imshow("contour", img)
+        cv2.waitKey()
         return img
 
-    def get_contour_parameters(self):
+    def get_contour_parameters(self, contour):
         """
         gets contour parameters used in shape analysis and saves them in class-level holders
         """
         # contour moments
-        M = cv2.moments(self.contour)
+        M = cv2.moments(contour)
         # contour centroid (c_x, c_y)
         self.centroid = (int(M['m10']/M['m00']), int(M['m01']/M['m00']))
-        self.cont_len = len(self.contour)
+        self.cont_len = len(contour)
 
-    def decimate_contour(self):
+    def decimate_contour(self, contour):
         """
         reduces number of contour points uniformly
         """
         sample_rate = 5
-        contour_decimated = []
-        i = 0
-        for point in self.contour:
-            if i == sample_rate:
-                contour_decimated.append(point)
-                i = 0
-            else:
-                i += 1
-        self.contour = contour_decimated
+        return contour[::sample_rate]
 
-    def get_grasp_points(self):
+    def get_grasp_points(self, contour):
         """
         returns a list of all potential grasp points pairs
         """
         grasp_points = []
-        min_pair_distance = int(self.cont_len / 5)
-        for i in range(self.cont_len - min_pair_distance):
-            for j in range(i + min_pair_distance, min((self.cont_len, i + self.cont_len - min_pair_distance + 1))):
-                grasp_points.append((self.contour[i], self.contour[j]))
+        # make local variable for speed
+        cont_len = self.cont_len
+        min_pair_distance = int(cont_len / 5)
+
+        for i in range(cont_len - min_pair_distance):
+            for j in range(i + min_pair_distance, min((cont_len, i + cont_len - min_pair_distance + 1))):
+                grasp_points.append((contour[i], contour[j]))
         return grasp_points
 
-    def get_centroid_distance(self, points):
+    def get_centroid_distance(self, points, centroid):
         """
         returns distance between center of grasp and object centroid
         :param points: pair of points (x,y) (tuple or list) defining grasp points
+        :param centroid: contour centroid (x,y)
         """
+
         grasp_center = ((points[0][0] + points[1][0])/2, (points[0][1] + points[1][1])/2)
-        distance = ((grasp_center[0] - self.centroid[0])**2 + (grasp_center[1] - self.centroid[1])**2)**0.5
+        distance = ((grasp_center[0] - centroid[0])**2 + (grasp_center[1] - centroid[1])**2)**0.5
         return distance
 
     def get_grasp_points_distance(self, points):
+        """
+        returns distance between 2 given points
+        """
         distance = ((points[0][0] - points[1][0])**2 + (points[0][0] - points[1][0])**2)**0.5
         return distance
+
+    def get_grasp_orientation(self, points):
+        """
+        returns grasp orientation in radians (-pi/2, pi/2]
+        """
+        line = (points[0][0] - points[1][0], points[0][1] - points[1][1])
+        if line[0] == 0:
+            # avoid division by 0
+            orientation = math.pi / 2
+        else:
+            # no need for atan2
+            orientation = math.atan(line[1] / line[0])
+        return orientation
