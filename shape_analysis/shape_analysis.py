2aeb81037d6506b940655a5b3de4d98c23384397
Kacperk6 <krejuchna12@gmail.com>
2020-07-24 00:38:37 +0200

    (WIP) added fitting line to curve, plotting, testing environment, changed point reference to index in contour

--- shape_analysis/shape_analysis.py
+++ shape_analysis/shape_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
 import math
+import matplotlib.pyplot as plt
 
 
 class ShapeAnalyzer:
@@ -9,7 +10,7 @@ class ShapeAnalyzer:
         self.cont_len = None
 
         # focal length of a camera in px, used for pixel-millimeter conversion
-        self.cam_focal_length = self.get_camera_focal_length(cam_mat)
+        # self.cam_focal_length = self.get_camera_focal_length(cam_mat)
 
         self.size_factor = None
 
@@ -44,7 +45,23 @@ class ShapeAnalyzer:
     def analyze_shape(self, mask, distance):
         contour = self.make_contour(mask, distance)
         self.draw_contour(contour, (int(mask.shape[0]*self.size_factor), int(mask.shape[1]*self.size_factor)))
-        grasp_points = self.get_grasp_points(contour)
+        grasp_points_idx_all = self.get_grasp_points_idx(contour)
+        self.save(contour, grasp_points_idx_all)
+        for i in range(len(grasp_points_idx_all)):
+            pass
+
+    def analyze_shape_test(self):
+        contour, grasp_points_idx_all = self.load_data()
+        # contour_x = contour[:, 0, 0]
+        # contour_y = contour[:, 0, 1]
+        # grasp_points_x = grasp_points[:, 0, 0]
+        # grasp_points_y = grasp_points[:, 0, 1]
+        for i in range(len(grasp_points_idx_all)):
+            grasp_points_idx = grasp_points_idx_all[i]
+            print(grasp_points_idx)
+            _ = plt.plot(contour[:, 0, 0], contour[:, 0, 1], 'b.', [contour[grasp_points_idx[0]][0][0], contour[grasp_points_idx[1]][0][0]], [contour[grasp_points_idx[0]][0][1], contour[grasp_points_idx[1]][0][1]], 'r-')
+            plt.show()
+            self.get_curves_orientation(contour, grasp_points_idx)
 
     @staticmethod
     def draw_contour(contour, img_size):
@@ -75,19 +92,19 @@ class ShapeAnalyzer:
         sample_rate = 5
         return contour[::sample_rate]
 
-    def get_grasp_points(self, contour):
+    def get_grasp_points_idx(self, contour):
         """
-        returns a list of all potential grasp points pairs
+        returns a list of indexes of all potential grasp point pairs
         """
-        grasp_points = []
+        grasp_points_idx = []
         # make local variable for speed
         cont_len = self.cont_len
         min_pair_distance = int(cont_len / 5)
 
         for i in range(cont_len - min_pair_distance):
             for j in range(i + min_pair_distance, min((cont_len, i + cont_len - min_pair_distance + 1))):
-                grasp_points.append((contour[i], contour[j]))
-        return grasp_points
+                grasp_points_idx.append((i, j))
+        return grasp_points_idx
 
     def get_centroid_distance(self, points, centroid):
         """
@@ -100,11 +117,11 @@ class ShapeAnalyzer:
         distance = ((grasp_center[0] - centroid[0])**2 + (grasp_center[1] - centroid[1])**2)**0.5
         return distance
 
-    def get_grasp_points_distance(self, points):
+    def get_points_distance(self, points):
         """
         returns distance between 2 given points
         """
-        distance = ((points[0][0] - points[1][0])**2 + (points[0][0] - points[1][0])**2)**0.5
+        distance = ((points[0][0] - points[1][0])**2 + (points[0][1] - points[1][1])**2)**0.5
         return distance
 
     def get_grasp_orientation(self, points):
@@ -137,3 +154,120 @@ class ShapeAnalyzer:
         f_y = cam_mat[1][1]
         f = (f_x + f_y) / 2
         return f
+
+    def get_curves_orientation(self, contour, grasp_points_idx):
+        """
+        returns mutual orientation of 2 curves about grasp points
+        """
+        def get_curve_line(contour, point_idx, grasp_vector):
+            gripper_size = 20
+
+            def get_curve_points():
+                """
+                returns points surrounding contour[point_idx] within gripper_size,
+                projected into UV (gripper) coordinate system
+                """
+                def project_point(point, coord_sys):
+                    """
+                    returns point projected to given coordinate system (u,v, origin(x,y))
+                    """
+                    return np.array([(point - coord_sys[2]) @ coord_sys[0], (point - coord_sys[2]) @ coord_sys[1]])
+                # middle curve point in original XY coord system
+                curve_points = contour[point_idx][0]
+                # curve point projected into UV coord system
+                curve_points_uv_new = np.array([project_point(curve_points, uv_coord_system)])
+                # backup array is needed, as the last one to pass test is returned
+                curve_points_uv = curve_points_uv_new
+                i = 1
+                # check value for contour length
+                curve_length_prev = 0
+                while True:
+                    # a circular iterator (itertools.cycle wasn't enough)
+                    last_idx = point_idx - i
+                    if last_idx < 0:
+                        last_idx = len(contour) + last_idx
+                    first_idx = point_idx + i
+                    if first_idx >= len(contour):
+                        first_idx = first_idx - len(contour)
+
+                    point_last = contour[last_idx][0]
+                    point_last_uv = project_point(point_last, uv_coord_system)
+                    point_first = contour[first_idx][0]
+                    point_first_uv = project_point(point_first, uv_coord_system)
+
+                    curve_points_uv_new = np.insert(curve_points_uv_new, 0, point_last_uv, axis=0)
+                    curve_points_uv_new = np.append(curve_points_uv_new, [point_first_uv], axis=0)
+                    # _ = plt.plot(curve_points_new[:, 0], curve_points_new[:, 1], 'b.')
+                    # plt.show()
+
+                    # length of U axis points projection
+                    # COŚ CHYBA Z OSIĄ U JEST NIE TAK
+                    curve_length = abs(curve_points_uv_new[len(curve_points_uv_new) - 1][0] - curve_points_uv_new[0][0])
+                    # stop adding points when length of curve projection (onto gripper) is greater than gripper size
+                    # or smaller than previous value (which happens when curve begins to close)
+                    if curve_length > gripper_size or curve_length < curve_length_prev:
+                        return curve_points_uv
+                    else:
+                        curve_length_prev = curve_length
+                        curve_points_uv = curve_points_uv_new
+                        i += 1
+
+            # local coord system definition
+            v = grasp_vector
+            u = np.array([v[1], -v[0]])
+            origin = (contour[point_idx][0])
+            uv_coord_system = (u, v, origin)
+
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_aspect(aspect=1)
+            ax.plot([p_0[0], p_1[0]], [p_0[1], p_1[1]], 'b.',
+                    [origin[0], origin[0] + u[0]], [origin[1], origin[1] + u[1]], 'r-',
+                    [origin[0], origin[0] + v[0]], [origin[1], origin[1] + v[1]], 'g-')
+            plt.show()
+
+            # points to fit line to, in UV coordinate system
+            curve_points = get_curve_points()
+            # line fit to points (least squares method)
+            line_coefficients = np.polyfit(curve_points[:, 0], curve_points[:, 1], 1)
+
+            line_eq = np.poly1d(line_coefficients)
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_aspect(aspect=1)
+            ax.plot(curve_points[:, 0], curve_points[:, 1], 'b.',
+                    [curve_points[0][0], curve_points[len(curve_points) - 1][0]],
+                    [line_eq(curve_points[0][0]), line_eq(curve_points[len(curve_points) - 1][0])], 'r-')
+            plt.show()
+
+            # direction = math.copysign( curve_points[len(curve_points)][0] - curve_points[0][0]
+            return line_coefficients
+
+        # grasp points given by indexes
+        p_0 = contour[grasp_points_idx[0]][0]
+        p_1 = contour[grasp_points_idx[1]][0]
+        # vector between given grasp points
+        grasp_points_vector = p_1 - p_0
+        # normalized grasp points vector (length = 1)
+        grasp_points_vector_norm = grasp_points_vector / self.get_points_distance((p_0, p_1))
+
+        # get lines fit to curves about both grasp points,
+        # local coordinate system bonded with gripper is used for line fitting
+        # local coord sys. vertical axis points outward contour
+        line_0 = get_curve_line(contour, grasp_points_idx[0], -grasp_points_vector_norm)
+        line_1 = get_curve_line(contour, grasp_points_idx[1], grasp_points_vector_norm)
+
+
+    def save(self, contour, grasp_points_idx_all):
+        np.savez_compressed('shape_analysis', contour=contour, grasp_points_idx_all=grasp_points_idx_all)
+
+    def load_data(self):
+        data = np.load('shape_analysis.npz')
+        contour = data['contour']
+        grasp_points_idx_all = data['grasp_points_idx_all']
+        self.get_contour_parameters(contour)
+        return contour, grasp_points_idx_all
+
+
+shape_analyzer = ShapeAnalyzer(None)
+shape_analyzer.analyze_shape_test()
